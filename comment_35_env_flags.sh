3fafd8b9a92ea881d6020c4d6034ace4dcb6e2f3
Marshall <38109796+mborrus@users.noreply.github.com>
2021-03-05 15:04:50 -0500

    Attempt to use the comment 35 solution

---
+++ comment_35_env_flags.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export FC=mpifort
+export F77=mpifort
+export CC=mpicc
+export CXX=mpiCC
+export FCFLAGS="-fcray-pointer -Waliasing -ffree-line-length-none -fno-range-check -O2 -g -W -fbounds-check  
+ -ffpe-trap=invalid,zero,overflow `nf-config --fflags ` "
+export CPPFLAGS="-O2 -g `nc-config --cflags ` "
+export CFLAGS="-O2 -g `nc-config --cflags ` "
+export LDFLAGS=" `nc-config --cflags 
