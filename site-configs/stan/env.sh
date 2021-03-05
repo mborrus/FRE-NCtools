ffa83b7ab54d50e55846b496b1f2142fbf6b49e6
Marshall <38109796+mborrus@users.noreply.github.com>
2021-03-05 12:29:19 -0500

    Update env.sh
    change last line

--- site-configs/stan/env.sh
+++ site-configs/stan/env.sh
@@ -7,11 +7,14 @@ module load netcdf/4.4.1.1
 module load hdf5/1.10.6
 module load nco/4.8.0
 module load ncl/6.6.2
-module load devel
 module load physics
-module load math
 
 # **********************************************************************
 # Set environment variables
 # **********************************************************************
-export LD_RUN_PATH=$LD_LIBRARY_PATH
+# export LD_RUN_PATH=$LD_LIBRARY_PATH <- ran it with this, got stuck at Fortran compilation in ./compile..
+
+# **********************************************************************
+# Set environment variables
+# **********************************************************************
+export CONFIG_SITE=/home/users/mborrus/FRE-NCtools/site-configs/stan/config.site
